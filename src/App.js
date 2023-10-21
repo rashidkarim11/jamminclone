d2ac48080742080a63633800d087a526d00bf9ef
umerrizwan1 <roxxumer2016@gmail.com>
2023-10-22 01:47:36 +0500

    Modified By Umer and Added Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Button from "./components/button";
 import Header from "./components/header";
 import Input from "./components/searchbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="bg-background w-full h-screen bg-cover">
+    <div className="bg-gradient-to-b from-green-500 to-black via-green-900 min-h-screen transition-all duration-500 ease-in-out">
       <Header />
       <div className="mt-20 flex flex-col justify-center items-center">
         <Input />
         <Button text="search" />
       </div>
+      <Footer />
     </div>
   );
 }
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,6 @@
 const Button = ({ text }) => {
   return (
-    <button class="cursor-pointer w-[8.11rem] py-2 px-3 rounded-full bg-[#010C3F] text-center text-[.833rem] transition-background-color-[.25s] border-0 text-white font-semibold">
+    <button className="cursor-pointer w-[8.11rem] py-2 px-3 rounded-full bg-gradient-to-r from-blue-900 to-indigo-600 text-[.833rem] transition duration-300 ease-in-out border-0 text-white font-semibold hover:bg-blue-800 focus:ring-2 focus:ring-blue-400">
       {text}
     </button>
   );
